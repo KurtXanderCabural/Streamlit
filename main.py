77187e943b7dbc0976e31ff5b2bbbc562193d966
KurtXanderCabural <120351490+KurtXanderCabural@users.noreply.github.com>
2024-08-30 13:37:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,37 @@ def web_portfolio():
     # page configs 
     st.set_page_config(page_title = "Kurt Xander Cabural",page_icon="⭐")
     # Set the page title
+    # Set the page title
     st.write(f"""
+    <style>
+    .waving-hand {{
+        display: inline-block;
+        animation: wave 1s linear infinite;
+    }}
+    
+    @keyframes wave {{
+        0% {{
+            transform: rotate(0deg);
+        }}
+        25% {{
+            transform: rotate(15deg);
+        }}
+        50% {{
+            transform: rotate(0deg);
+        }}
+        75% {{
+            transform: rotate(-15deg);
+        }}
+        100% {{
+            transform: rotate(0deg);
+        }}
+    }}
+    </style>
     <div class="title" style="text-align: center;">
-    <span style='font-size: 32px;'>Hello! My name is Abhisheak Saraswat</span>👋
+    <span style='font-size: 32px;'>Hello! My name is Kurt Xander Cabural</span>
+    <span class="waving-hand">👋</span>
     </div>
     """, unsafe_allow_html=True)
-    st.markdown('<style>div.block-container{padding-top:3rem;}</style>',unsafe_allow_html=True)
-
-   
 
 if __name__ == "__main__":
     web_portfolio()
