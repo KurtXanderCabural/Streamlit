75e85e9246ac9caeb5eae712229f7debb0f3d7c1
KurtXanderCabural <120351490+KurtXanderCabural@users.noreply.github.com>
2024-08-31 00:06:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,26 @@ def web_portfolio():
         
     else:
         st.sidebar.write("")  # This ensures nothing is shown when the info is hidden
+
+    with st.container():
+    st.subheader('⚒️ Skills')
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
+    with col1:
+        st_lottie(python_lottie, height=70,width=70, key="python", speed=2.5)
+    with col2:
+        st_lottie(java_lottie, height=70,width=70, key="java", speed=4)
+    with col3:
+        st_lottie(my_sql_lottie,height=70,width=70, key="mysql", speed=2.5)
+    with col4:
+        st_lottie(git_lottie,height=70,width=70, key="git", speed=2.5)
+    with col1:
+        st_lottie(github_lottie,height=50,width=50, key="github", speed=2.5)
+    with col2:
+        st_lottie(docker_lottie,height=70,width=70, key="docker", speed=2.5)
+    with col3:
+        st_lottie(figma_lottie,height=50,width=50, key="figma", speed=2.5)
+    with col4:
+        st_lottie(js_lottie,height=50,width=50, key="js", speed=1)
         
     # Set the page title with waving hand emoji animation
     st.write(f"""
